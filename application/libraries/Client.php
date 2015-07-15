143828ab68a70f46ef9f45ca183db82c09ce3633
JackSun <sunxguo@163.com>
2015-07-15 09:55:10 +0800

    update

---
+++ application/libraries/Client.php
@@ -0,0 +1,51 @@
+<?php 
+	/**
+	 * 客户端代码
+	 */
+	 
+	error_reporting(0);
+	//确保在连接客户端时不会超时
+	set_time_limit(0);
+	//设置IP和端口号
+	$address = "localhost";
+	$port = 1935;
+	echo " TCP/IP Connection /n";
+	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	if ($socket === false)
+	{
+		die;
+	}
+	else
+	{
+		echo "OK";
+	}
+	echo "试图连接 ";
+	if (socket_connect($socket, $address, $port) == false)
+	{
+		$error = socket_strerror(socket_last_error());
+		echo "socket_connect() failed./n","Reason: {$error} /n";
+		die;
+	}
+	else
+	{
+		echo "连接OK/n";
+	}
+	$in   = "Hello World/r/n";
+	if (socket_write($socket, $in, strlen($in)) === false)
+	{
+		echo "socket_write() failed: reason: " . socket_strerror(socket_last_error()) ."/n";
+		die;
+	}
+	else
+	{
+		echo "发送到服务器信息成功！/n","发送的内容为: $in  /n";
+	}
+	$out  = "";
+	while ($out = socket_read($socket, 8192))
+	{
+		echo "接受的内容为: ".$out;
+	}
+	echo "关闭SOCKET…/n";
+	socket_close($socket);
+	echo "关闭OK/n";
+?>
