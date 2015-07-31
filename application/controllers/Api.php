347855a2d5a06187985e98c07f2d1a159a21aff6
MonkeyKing <MonkeyKing1024@gmail.com>
2015-07-31 17:07:28 +0800

    update

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -6,7 +6,7 @@ class Api extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->library('Common');
-		$this->load->model("DbHandler");
+		$this->load->model("Dbhandler");
 	}
 	public function index(){
 		
@@ -50,7 +50,7 @@ class Api extends CI_Controller {
 			echo json_encode($echoData);
 			return false;
 		}
-		$this->DbHandler->insertData('user',array(
+		$this->Dbhandler->insertData('user',array(
 			'email'=>$_POST['email'],
 			'password'=>md5('QppMK'.$_POST['password']),
 			'gender'=>2,
@@ -110,7 +110,7 @@ class Api extends CI_Controller {
 			'where'=>array('id'=>$user->id),
 			'data'=>array('device'=>$_POST['device'],'token'=>$token,'token_exptime'=>$tokenExptime)
 		);
-		$this->DbHandler->updateData($updateData);
+		$this->Dbhandler->updateData($updateData);
 		//if(time()>$user->token_exptime)
 		$data=new stdClass;
 		$data->token=$token;
@@ -163,7 +163,7 @@ class Api extends CI_Controller {
 			echo json_encode($echoData);
 			return false;
 		}
-		$this->DbHandler->insertData('contact',array(
+		$this->Dbhandler->insertData('contact',array(
 			'userId'=>$userId,
 			'contactId'=>$_POST['contactId']
 		));
--- application/libraries/Common.php
+++ application/libraries/Common.php
@@ -4,7 +4,7 @@ class Common{
 	var $CI;
 	function __construct(){
 		$this->CI =& get_instance();
-		$this->CI->load->model("DbHandler");
+		$this->CI->load->model("Dbhandler");
 	}
 	
 	/**
@@ -35,7 +35,7 @@ class Common{
 		return $this->getOneData($condition);
 	}
 	public function getData($condition){
-		return $this->CI->dbHandler->selectData($condition);
+		return $this->CI->DbHandler->selectData($condition);
 	}
 	public function isExist($table,$where){
 		$result=false;
